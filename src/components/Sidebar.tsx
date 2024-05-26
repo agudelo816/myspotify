d33737c79e15f4593f5856f922ddb5a58a47c859
Daniel Agudelo <d.agudelo816@gmai.com>
2024-05-26 12:32:29 -0400

    removed sidebar search

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
-// import { ResizableBox, ResizeCallbackData } from 'react-resizable';
 import { Resizable, ResizeCallback, ResizeDirection } from 're-resizable';
 
 import './Sidebar.scss';
 
 const Sidebar: React.FC = () => {
-  // const [width, setWidth] = useState(200); // Initial width of the sidebar
   const [size, setSize] = useState({ width: 200, height: '100%' });
   const handleResizeStop: ResizeCallback = (e, direction, ref, d) => {
     setSize({
@@ -13,17 +11,6 @@ const Sidebar: React.FC = () => {
       height: size.height,
     });
   };
-
-  // const handleResize = (event, { size }) => {
-  //   setWidth(size.width);
-  // };
-
-  // const handleResize = (
-  //   event: React.SyntheticEvent<Element>,
-  //   { size }: ResizeCallbackData
-  // ) => {
-  //   setWidth(size.width);
-  // };
 
   return (
     <Resizable
@@ -43,9 +30,6 @@ const Sidebar: React.FC = () => {
           <button className="control-button">Download</button>
         </div>
       </div>
-      <div className="sidebar-search">
-        <input type="text" placeholder="Search" className="search-input" />
-      </div>
       <div className="playlist-list">
         <div className="playlist-item">
           <i className="fas fa-heart"></i>
@@ -63,52 +47,6 @@ const Sidebar: React.FC = () => {
       </div>
     </Resizable>
   );
-  // return (
-  //   <ResizableBox
-  //     width={width}
-  //     height={Infinity}
-  //     axis="x"
-  //     minConstraints={[150, Infinity]}
-  //     maxConstraints={[500, Infinity]}
-  //     onResize={handleResize}
-  //     resizeHandles={['e']}
-  //     className="sidebar"
-  //   >
-  //     <div className="sidebar">
-  //       <div className="sidebar-header">
-  //         <h2>Your Library</h2>
-  //         <div className="sidebar-controls">
-  //           <button className="control-button">Playlists</button>
-  //           <button className="control-button">By you</button>
-  //           <button className="control-button">By Spotify</button>
-  //           <button className="control-button">Download</button>
-  //         </div>
-  //       </div>
-  //       <div className="sidebar-search">
-  //         <input
-  //           type="text"
-  //           placeholder="Search Playlists"
-  //           className="search-input"
-  //         />
-  //       </div>
-  //       <div className="playlist-list">
-  //         <div className="playlist-item">
-  //           <i className="fas fa-heart"></i>
-  //           <span>Liked Songs</span>
-  //         </div>
-  //         <div className="playlist-item">
-  //           <i className="fas fa-podcast"></i>
-  //           <span>Your Episodes</span>
-  //         </div>
-  //         <div className="playlist-item">
-  //           <i className="fas fa-folder"></i>
-  //           <span>EDC</span>
-  //         </div>
-  //         {/* Add more playlist items as needed */}
-  //       </div>
-  //     </div>
-  //   </ResizableBox>
-  // );
 };
 
 export default Sidebar;
